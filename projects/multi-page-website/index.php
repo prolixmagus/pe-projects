8baea1f81e1b4d5386bbf01a2944d90affc36f7b
prolixmagus <jeremypfishman@gmail.com>
2023-12-23 13:49:34 -0800

    Move files to a subfolder+
    From pe-projects to pe-projects/projects

---
+++ projects/multi-page-website/index.php
@@ -0,0 +1,19 @@
+<?php include('header.php') ?>
+
+			<section class='main inner-column'>
+				<h1>Welcome!</h1>
+
+				<p>I'm so glad you're here!</p>
+
+				<p>I made this for you. It's a flower</p>
+
+				<p>Once you're done contemplating the flower, check the links at the top of the page</p>
+
+				<picture>
+					<img src='https://peprojects.dev/alpha-8/jeremy/projects/multi-page-website/purple-flower.jpg' alt ='purple flower overlooking the ocean'>
+				</picture>
+
+				<p>Did you do it?</p>
+			</section>
+
+<?php include('footer.php')?>
---
+++ projects/multi-page-website/learning-journal.php
@@ -0,0 +1,20 @@
+<?php include('header.php') ?>
+
+			<article>
+				<section class='learning inner-column'>
+
+					<h1>My Learning Journal</h1>
+
+					<div class='learning-content'>
+
+						<p>Things are going pretty well so far. Trying to do things by the program, so going back to Sublime Text after a stint with VSCode. This is a nice review of HTML - feels good to practice.</p>
+
+						<p>I'm trying not to tackle too many things at once, but I feel there is <i>so</i> much to catch up on. I want to try and make one Affinity doodle a day, or learn one new little trick in Affinity. I also want to keep brainstorming ideas, since I know there's the spectre of the <b>big</b> project later in the course. I'm glad that I'm nurturing it now.</p>
+
+						<p>The hardest thing right now is to get through my part-time tutoring job, since I'd rather be at home tinkering around! I think P.E. should send out stickers. Stickers are always good.</p>
+					</div>
+				</section>
+			</article>
+		</main>
+
+<?php include('footer.php') ?>
