0a43e574f170f0f535aed39e6b0eae491d237451
prolixmagus <jeremypfishman@gmail.com>
2024-01-22 23:49:31 -0800

    Adjust logic in counting characters form

--- projects/exercises-for-programmers/counting-characters/counting-characters-form.php
+++ projects/exercises-for-programmers/counting-characters/counting-characters-form.php
@@ -12,44 +12,57 @@
 
 ?>
 
-<a class="refresh" href="?">Counting Characters</a>
+<main>
 
-<inner-column>
+	<section class="form-group">
+		<form-explanation>
+			<inner-column>
+				<h2 class="form-title"><a class="refresh" href="?">Counting Characters</a></h2>
+				<p class="indoor-voice">PHP technically does not count the characters in a given string, rather, the amount of bytes that make up the string. Usually byte to character is 1:1, but certain languages (like Arabic or Chinese) are several bytes per character!</p>
+				<p class="indoor-voice">TL;DR I made a form to count the letters in a word.</p>
+			</inner-column>
+		</form-explanation>
 
-	<form method='POST'>
-		<h2>Input</h2>
-		
-		<div class='field'>
-			<label for='string'>Please input a string:</label>
-			<input type='text' id='string' name='string' value='<?=$string?>'>
-		</div>
+	<form-section>
+		<inner-column>
+				
+				<form method="POST">
 
-		<action-buttons>
-			<button class='submit-button' type='submit' name='submitted'>
-				Submit
-			</button>
+					<h2>Input</h2>
 
-			<button class='clear-button' type='submit' name='clear'>
-				Clear
-			</button>
-		</action-buttons>
+					<div class="field">
+						<label for="string">Write anything!</label>
+						<input id ="string" type="text" name="string" value="<?=$string?>" required />
+					</div>
 
-	</form>
+					<action-buttons>
 
-</inner-column>
+						<button class="submit-button" type="submit" name="submitted">
+							Submit
+						</button>
 
-<inner-column>
+						<button class="clear-button" type="submit" name="clear">
+							clear x
+						</button>
 
-	<output>
-		<h2>Output</h2>
-		<div>
-			<p class="message"><?=$message?></p>
-		</div>
-	</output>
+					</action-buttons>
+				</form>
 
-</inner-column>
+			</inner-column>
+		</form-section>
+	</section>
 
-<?php
-displayCode($_POST);
-?>
+	<?php if (isset($_POST["submitted"]) ) { ?>
+		<section class="form-output">
+			<inner-column>
 
+					<output>
+						<h2>Output</h2>
+						<p class="output-voice"><?=$message?></p>
+					</output>
+			
+			</inner-column>
+		</section>
+	<?php } ?>
+	
+</main>
