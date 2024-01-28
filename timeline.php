e08d171e7b99922d49ac2c827bcaab65859b3b55
prolixmagus <jeremypfishman@gmail.com>
2024-01-27 16:57:11 -0800

    Change styles on timeline page

--- timeline.php
+++ timeline.php
@@ -1,235 +1,177 @@
-<!DOCTYPE html>
-<html lang='en'>
-	<head>
-		<meta charset='UTF-8'>
-		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+<section class='intro'>
+	<inner-column>
+		<h2 class='title'>Everything I remember so far</h2>
+
+	</inner-column>
+</section>
+
+<section class='timeline' id='present-day'>
+	<inner-column class='first'>
+		<div>
+			<h2>2024</h2>
+
+			<p>I am currently enrolled in a web development course by <a class='derek-pink' href='https://perpetual.education/'>Perpetual Education</a> called Design for <em>the Web</em>.</p>
+
+			<p>It's a lot of work. I love it.</p>
+
+			<p>Aside from researching and applying new web technologies, I love reading (or writing) short stories and playing jazz. Feel free to check out what I'm currently reading, listening to, or watching.</p>
+		</div>
+
+		<experience-gained>
+			<h3>skills</h3>
+
+			<ul title='skills so far'>
+				<li>Research and study through <a href='https://www.sciencedirect.com/topics/psychology/deliberate-practice'>Deliberate Practice</a></li>
+				<li>Preparing shareable rich metadata assets</li>
+				<li>Creating and exporting original graphics using Affinity 2 design software</li>
+				<li>Formatting responsive webpages for accessibility via semantic HTML, typography, and usability concerns</li>
+				<li>HTML + CSS + PHP</li>
+			</ul>
+		</experience-gained>
+
+	</inner-column>
+</section>
+
+<section class='timeline' id='burbank'>
+	<inner-column>
+		<div>
+			<h2>2022</h2>
+			
+			<p>Four years and one pandemic later, I am flying across the country to teach at John Muir Middle School in Burbank, California.</p>
+
+		</div>
+		<experience-gained>
+			<h3>skills</h3>
+			
+			<ul>
+				<li>Designed over 185 unique standards-based lesson plans</li>
+				<li>Tracked student progress / behavior for ~100 English Language Learners</li>
+				<li>Developed academic and behavior intervention plans alongisde counselors and school psychologists</li>
+				<li>Proctored high stakes state tests (CAASPP and ELPAC)</li>
+				<li>Shared ELD best practices with Math, Science, and History departments</li>
+			</ul>
+		</experience-gained>
+	</inner-column>
+</section>
+
+<section class='timeline' id='white-oak'>
+	<inner-column>
+		<div>
+			<h2>2019</h2>
+			
+			<p>I'm back in the United States teaching English as a Second language to immigrants from all over the world. I'm teaching at White Oak Middle School in Maryland.</p>
+
+		</div>
+		<experience-gained>
+			<h3>skills</h3>
+			
+			<ul>
+				<li>Co-taught STEM courses as ELD support</li>
+				<li>Created language curriculum for ELD levels 1-4 and 6th-8th ELA classes.</li>
+				<li>Proctored WIDA assessment to place incoming and current students</li>
+			</ul>
+		</experience-gained>
+	</inner-column>
+</section>
+
+	<!-- insert section for Burbank Middle School / Sylvan and White Oak -->
+
+<section class='timeline'>
+	<inner-column>
+		<div>
+			<h2>2018</h2>
+			
+			<p>I am in teaching in English in Taiwan as the recipient of a Fulbright Scholarship. I travel to different elementary schools in Zhiben, a spa town in the Taitung region.</p>
+
+			<p>Here is an archive of a blog I wrote during my stay</p>
+			
+			
+			<!-- insert link here -->
+		</div>
+		<experience-gained>
+			<h3>skills</h3>
+			
+			<ul>
+				<li>Co-taught grades K-6 EFL classes with a certified local English teacher</li>
+				<li>Initiated and delivered assessments to improve student cooperation, critical thinking, and communication skills</li>
+				<li>Participated in local and government events as a cultural ambassador</li>
+			</ul>
+		</experience-gained>
+	</inner-column>
+</section>
+
+
+<section class='timeline'>
+	<inner-column>
+		<div>
+			<h2>2017</h2>
+			
+			<p>After three years, I graduate from Montclair State University with a Master's in Teaching English as a Second Language. I am one course shy of a Master's in Applied Linguistics (statistics)</p>
+			
+			<p>Post-graduation, I take the train to Princeton, <abbr title='New Jersey'>NJ</abbr>, to write test questions for the <abbr title='Test of English as a Foreign Language'>TOEFL</abbr> <abbr title='internet-Based-Test'>iBT</abbr> at <abbr title='Educational Testing Services'>ETS</abbr> over the summer.</p>
+		</div>
+		<experience-gained>
+			<h2>skills</h2>
+			
+			<ul>
+				<li>Researched, created, and revised test items for the Speaking Section of the TOEFL iBT</li>
+				<li>Researched and cross-referenced data</li>
+				<li>Safeguarded confidential testing assets</li>
+			</ul>
+		</experience-gained>
+	</inner-column>
+</section>
+
+
+<section class='timeline'>
+	<inner-column>
+		<div>
+			<h2>2014</h2>
+			
+			<p>I disembark a plane that just landed in Seattle, Washington, to search for new prospects in the midst of a recession. A few months later and I am working the help line at Nintendo of America's call center.</p>
+			
+			<p>I soon get promoted to become an Associate Data Analyst.</p> 
+			
+			<p>I feel stuck. I miss my family. I move back home</p>
+		</div>
+		<experience-gained>
+			<h3>skills</h3>
+			
+			<ul>
+				<li>Compiled feedback from tech forums, social media, news outlets, and the Miiverse into digestible reports for upper management.</li>
+				<li>Created data-driven flash reports tracking user reactions to software updates and press releases.</li>
+			</ul>
+		</experience-gained>
+	</inner-column>
+</section>
 		
-		<meta property='og:title' content='Fishman: Timeline'>
-		<meta property='og:type' content='article'>
-		<meta property='og:description' content='Teacher. Web Developer. Linguist. Writer. Human.'>
+<section class='timeline'>
+	<inner-column class='last'>
+		<div>
+			
+			<h2>2011</h2>
+			
+			<p>Clutching a degree in Creative Writing from the University of Rochester and a certificate from NYU's Summer Publishing Institute, I set out to realize my dreams of becoming an Editor.</p>
+			
+			<p>For one year, I am an Assistant Editor of E-Books at Skyhorse Publishing.</p> 
+			
+			<p>This is the year the publishing industry implodes.</p>
 
-		<meta property='og:url' content='https://www.peprojects.dev/alpha-8/jeremy/timeline.html'>
-		<meta property='og:image' content='www.peprojects.dev/alpha-8/jeremy/images/meta-fish-card.jpg'>
+		</div>
+		<experience-gained>
+			
+			<h3>skills</h3>
+			
+			<ul>
+				<li>Proofread and reformatted 60 titles per month for online distribution to vendors such as Amazon, Barnes & Noble, and iTunes Bookstore</li>
+				<li>Compiled and distributed Metadata excel sheets for over 100 vendors through FTP</li>
+				<li>Tracked sales and inventory from digital marketplace using iTunes Producer and Amazon’s Vendor Central</li>
+			</ul>
+		</experience-gained>
+	</inner-column>	
+</section>
 
-		<meta name="twitter:title" content="Jeremy Fishman's Portfolio">
-		<meta name="twitter:description" content="Teacher. Web Developer. Linguist. Writer. Human.">
-		<meta name="twitter:image" content="www.peprojects.dev/alpha-8/jeremy/images/meta-fish-card.jpg">
-		<meta name="twitter:card" content="summary_large_image">
-
-		<title>Jeremy Fishman</title>
-
-		<link rel='stylesheet' href='css/style.css'>
-
-	</head>
-
-	<body>
-
-		<header class='site-header'>
-			<inner-column>
-
-				<?php include("site-header.php")?>
-				
-			</inner-column>
-		</header>
-
-		<main class='intro'>
-			<section>
-				<inner-column>
-					<h2 class='title'>Everything I remember so far</h2>
-
-				</inner-column>
-			</section>
-
-			<section class='timeline' id='present-day'>
-				<inner-column class='first'>
-					<div>
-						<h2>2024</h2>
-
-						<p>I am currently enrolled in a web development course by <a class='derek-pink' href='https://perpetual.education/'>Perpetual Education</a> called Design for <em>the Web</em>.</p>
-
-						<p>It's a lot of work. I love it.</p>
-
-						<p>Aside from researching and applying new web technologies, I love reading (or writing) short stories and playing jazz. Feel free to check out what I'm currently reading, listening to, or watching.</p>
-					</div>
-
-					<experience-gained>
-						<h3>skills</h3>
-
-						<ul title='skills so far'>
-							<li>Research and study through <a href='https://www.sciencedirect.com/topics/psychology/deliberate-practice'>Deliberate Practice</a></li>
-							<li>Preparing shareable rich metadata assets</li>
-							<li>Creating and exporting original graphics using Affinity 2 design software</li>
-							<li>Formatting responsive webpages for accessibility via semantic HTML, typography, and usability concerns</li>
-							<li>HTML + CSS + PHP</li>
-						</ul>
-					</experience-gained>
-
-				</inner-column>
-			</section>
-
-			<section class='timeline' id='burbank'>
-				<inner-column>
-					<div>
-						<h2>2022</h2>
-						
-						<p>Four years and one pandemic later, I am flying across the country to teach at John Muir Middle School in Burbank, California.</p>
-
-					</div>
-					<experience-gained>
-						<h3>skills</h3>
-						
-						<ul title='skills'>
-							<li>Designed over 185 unique standards-based lesson plans</li>
-							<li>Tracked student progress / behavior for ~100 English Language Learners</li>
-							<li>Developed academic and behavior intervention plans alongisde counselors and school psychologists</li>
-							<li>Proctored high stakes state tests (CAASPP and ELPAC)</li>
-							<li>Shared ELD best practices with Math, Science, and History departments</li>
-						</ul>
-					</experience-gained>
-				</inner-column>
-			</section>
-
-			<section class='timeline' id='white-oak'>
-				<inner-column>
-					<div>
-						<h2>2019</h2>
-						
-						<p>I'm back in the United States teaching English as a Second language to immigrants from all over the world. I'm teaching at White Oak Middle School in Maryland.</p>
-
-					</div>
-					<experience-gained>
-						<h3>skills</h3>
-						
-						<ul title='skills'>
-							<li>Co-taught STEM courses as ELD support</li>
-							<li>Created language curriculum for ELD levels 1-4 and 6th-8th ELA classes.</li>
-							<li>Proctored WIDA assessment to place incoming and current students</li>
-						</ul>
-					</experience-gained>
-				</inner-column>
-			</section>
-
-				<!-- insert section for Burbank Middle School / Sylvan and White Oak -->
-
-			<section class='timeline'>
-				<inner-column>
-					<div>
-						<h2>2018</h2>
-						
-						<p>I am in teaching in English in Taiwan as the recipient of a Fulbright Scholarship. I travel to different elementary schools in Zhiben, a spa town in the Taitung region.</p>
-
-						<p>Here is an archive of a blog I wrote during my stay</p>
-						
-						
-						<!-- insert link here -->
-					</div>
-					<experience-gained>
-						<h3>skills</h3>
-						
-						<ul title='skills'>
-							<li>Co-taught grades K-6 EFL classes with a certified local English teacher</li>
-							<li>Initiated and delivered assessments to improve student cooperation, critical thinking, and communication skills</li>
-							<li>Participated in local and government events as a cultural ambassador</li>
-						</ul>
-					</experience-gained>
-				</inner-column>
-			</section>
-
-
-			<section class='timeline'>
-				<inner-column>
-					<div>
-						<h2>2017</h2>
-						
-						<p>After three years, I graduate from Montclair State University with a Master's in Teaching English as a Second Language. I am one course shy of a Master's in Applied Linguistics (statistics)</p>
-						
-						<p>Post-graduation, I take the train to Princeton, <abbr title='New Jersey'>NJ</abbr>, to write test questions for the <abbr title='Test of English as a Foreign Language'>TOEFL</abbr> <abbr title='internet-Based-Test'>iBT</abbr> at <abbr title='Educational Testing Services'>ETS</abbr> over the summer.</p>
-					</div>
-					<experience-gained>
-						<h2>skills</h2>
-						
-						<ul title='skills'>
-							<li>Researched, created, and revised test items for the Speaking Section of the TOEFL iBT</li>
-							<li>Researched and cross-referenced data</li>
-							<li>Safeguarded confidential testing assets</li>
-						</ul>
-					</experience-gained>
-				</inner-column>
-			</section>
-
-
-			<section class='timeline'>
-				<inner-column>
-					<div>
-						<h2>2014</h2>
-						
-						<p>I disembark a plane that just landed in Seattle, Washington, to search for new prospects in the midst of a recession. A few months later and I am working the help line at Nintendo of America's call center.</p>
-						
-						<p>I soon get promoted to become an Associate Data Analyst.</p> 
-						
-						<p>I feel stuck. I miss my family. I move back home</p>
-					</div>
-					<experience-gained>
-						<h3>skills</h3>
-						
-						<ul title='skills'>
-							<li>Compiled feedback from tech forums, social media, news outlets, and the Miiverse into digestible reports for upper management.</li>
-							<li>Created data-driven flash reports tracking user reactions to software updates and press releases.</li>
-						</ul>
-					</experience-gained>
-				</inner-column>
-			</section>
-					
-			<section class='timeline'>
-				<inner-column>
-					<div>
-						
-						<h2>2011</h2>
-						
-						<p>Clutching a degree in Creative Writing from the University of Rochester and a certificate from NYU's Summer Publishing Institute, I set out to realize my dreams of becoming an Editor.</p>
-						
-						<p>For one year, I am an Assistant Editor of E-Books at Skyhorse Publishing.</p> 
-						
-						<p>This is the year the publishing industry implodes.</p>
-
-					</div>
-					<experience-gained>
-						
-						<h3>skills</h3>
-						
-						<ul title='skills'>
-							<li>Proofread and reformatted 60 titles per month for online distribution to vendors such as Amazon, Barnes & Noble, and iTunes Bookstore</li>
-							<li>Compiled and distributed Metadata excel sheets for over 100 vendors through FTP</li>
-							<li>Tracked sales and inventory from digital marketplace using iTunes Producer and Amazon’s Vendor Central</li>
-						</ul>
-					</experience-gained>
-				</inner-column>	
-			</section>
-					
-					
-			<section class='timeline'>
-				<inner-column>
-					<div>
-						
-						<h2>2010</h2>
-						
-						<p>I am working for my father's audio production company, <a href='https://www.holdcom.com/'>Holdcom</a>. I shuffle under waveforms and track down stray sibilants.</p>
-					</div>
-					<experience-gained>
-						<h3>skills</h3>
-
-						<ul title='skills'>
-							<li>Audacity (software)</li>
-							<li>Marketing</li>
-							<li>Read and interpreted various SEO Analytics</li>
-						</ul>
-					</experience-gained>
-
-				</inner-column>	
-			</section>
-			<picture class='timeline-seaweed'>
-				<img src='images/seaweed-1.svg' alt='pink seaweed'>
-			</picture>
-		</main>
-	</body>
-</html>
+<picture class='timeline-seaweed'>
+	<img src='images/seaweed-1.svg' alt='pink seaweed'>
+</picture>
