ecc797ff6a3728b6a624f61137481bee53f95f3f
prolixmagus <jeremypfishman@gmail.com>
2024-03-03 11:56:32 -0800

    Create goals page and stylesheet
    .

--- pages/home/template.php
+++ pages/home/template.php
@@ -4,7 +4,7 @@
 	
 	<p class='calm-voice'>I'm Jeremy, a web developer and K-12 <abbr title='English as a Second Language'>ESL</abbr> educator. I help people of all ages navigate foreign systems.</p>
 
-	<p class='calm-voice'>If I could sum up the 21st century in a single word, I would choose <span>overwhelming</span>. My goal as a developer is to untangle the knots while preserving joy in discovery. There will be no more <span>whelming</span> on my watch!</p>
+	<p class='calm-voice'>If I could sum up the 21st century in a single word, I would choose <span>overwhelming</span>. My <a href="?page=goals">goal as a developer</a> is to untangle the knots while preserving joy in discovery. There will be no more <span>whelming</span> on my watch!</p>
 
 	<p class='calm-voice'>If you want to talk tech, music, linguistics, or whatever crosses your mind, you can reach me at <a href='mailto:jeremypfishman@gmail.com'>jeremypfishman@gmail.com</a>. I usually respond within 24 hours.</p>
 
