21be47c6afa36d1cfcb7288ec75af4447a1ea315
prolixmagus <jeremypfishman@gmail.com>
2024-03-06 00:12:53 -0800

    Add overview section css html and update database

---
+++ projects/site-mockup-to-code/templates/modules/overview/template.php
@@ -0,0 +1,23 @@
+<overview-container>
+	<company-overview>
+		<h2 class="attention-voice"><?=$section['heading'];?></h2>
+		<p class="calm-voice"><?=$section['sub-heading'];?></p>
+		<picture>
+			<img src="<?=$section['image']?>" />
+		</picture>
+	</company-overview>
+	<article-gallery>
+		<?php foreach($section['article'] as $article) { ?>
+			<article-card class='<?=$article['article-card']?>'>
+				<?php include('templates/components/icon.php')?>
+					<article-content>
+						<h2 class="small-heading-voice"><?=$article['small-heading']?></h2>
+						<p class="calm-voice"><?=$article['article-text']?></p>
+					</article-content>
+			</article-card>
+		<?php } ?>
+
+	</article-gallery>
+
+</overview-container>
+
