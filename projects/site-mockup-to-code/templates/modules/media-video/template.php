daca5f0a999e5939f797f9578e5c8adba7eb5a3f
prolixmagus <jeremypfishman@gmail.com>
2024-03-05 21:21:25 -0800

    Create conditional statement to display iframe based on page

--- projects/site-mockup-to-code/templates/modules/media-video/template.php
+++ projects/site-mockup-to-code/templates/modules/media-video/template.php
@@ -1,9 +1,9 @@
 <video-heading>
-	<p class="calm-voice"><?=$section['teaser']?></p>
-	<h2 class="attention-voice"><?=$section['heading']?></h2>
-	<p class="calm-voice"><?=$section['sub-heading']?></p>
+	<p class="calm-voice"><?=$section['teaser'];?></p>
+	<h2 class="attention-voice"><?=$section['heading'];?></h2>
+	<p class="calm-voice"><?=$section['sub-heading'];?></p>
 </video-heading>
-<div>
-	<iframe width="510" height="300" src="<?=$section['video']?>" title="Clouds 4K free footage" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+<?php if ($page == 'home') { ?>
+	<iframe width="510" height="300" src="<?=$section['video'];?>" title="Clouds 4K free footage" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
 	</iframe>
-<div>
+<?php } ?>
