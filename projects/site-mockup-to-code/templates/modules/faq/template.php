72916ccf9d04a3f03de90268dff466b061704f73
prolixmagus <jeremypfishman@gmail.com>
2024-03-07 02:06:27 -0800

    Add faq modules and update json data

---
+++ projects/site-mockup-to-code/templates/modules/faq/template.php
@@ -0,0 +1,14 @@
+<faq-info>
+	<h2 class='attention-voice'><?=$section['heading']?></h2>
+	<p class='calm-voice'><?=$section['sub-heading']?></p>
+</faq-info>
+<ul class='question-list'>
+	<?php foreach($section['details'] as $detail) { ?>
+		<li class='details-item'>
+			<details>
+				<summary class='strong-voice weighted'><?=$detail['question']?></summary>
+				<p class='quiet-voice'><?=$detail['answer']?></p>
+			</details>
+		</li>
+	<?php } ?>
+</ul>
