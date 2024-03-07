1e9e3ac8fe1d95e6053f31b65add7c4e2f5e181e
prolixmagus <jeremypfishman@gmail.com>
2024-03-06 17:42:52 -0800

    Create star svg for ratings

---
+++ projects/site-mockup-to-code/templates/components/star.php
@@ -0,0 +1,6 @@
+<svg width="100%" height="100%" viewBox="0 0 110 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(1,0,0,1,-517.39,-729.291)">
+        <path d="M572.372,729.291L585.351,769.237L627.353,769.237L593.373,793.926L606.352,833.872L572.372,809.184L538.391,833.872L551.37,793.926L517.39,769.237L559.392,769.237L572.372,729.291Z"/>
+    </g>
+</svg>
+
