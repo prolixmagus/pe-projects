92eba2f2be708b37466a8317338d8440d6a8dfee
prolixmagus <jeremypfishman@gmail.com>
2024-03-08 01:09:58 -0800

    Finish page-footer HTML template and CSS stylesheet

--- projects/site-mockup-to-code/templates/modules/page-footer/template.php
+++ projects/site-mockup-to-code/templates/modules/page-footer/template.php
@@ -1,80 +1,84 @@
-<?php foreach ($pageData['page-footer'] as $content) { ?>
-<site-map>
-	<nav class='site-menu product'>
-		<?php foreach($content['product-links'] as $link) { ?>
-			<h3><?=$link['heading']?></h3>
-			<ul>
-				<?php foreach($link['links'] as $link_name) { ?>
-					<li>
-						<a href='#'><?=$link_name['name']?>
-						</a>
-					</li>
+
+<inner-column>
+	<?php foreach ($pageData['page-footer'] as $content) { ?>
+		<site-map>
+			<nav class='site-menu product'>
+				<?php foreach($content['product-links'] as $link) { ?>
+					<h3 class='calm-voice'><?=$link['heading']?></h3>
+					<ul>
+						<?php foreach($link['links'] as $link_name) { ?>
+							<li>
+								<a href='#'><?=$link_name['name']?>
+								</a>
+							</li>
+						<?php } ?>
+					</ul>
 				<?php } ?>
-			</ul>
-		<?php } ?>
-	</nav>
+			</nav>
 
-	<nav class='site-menu information'>
-		<?php foreach($content['information-links'] as $link) { ?>
-			<h3><?=$link['heading']?></h3>
-			<ul>
-				<?php foreach($link['links'] as $link_name) { ?>
-					<li>
-						<a href='#'><?=$link_name['name']?>
-						</a>
-					</li>
+			<nav class='site-menu information'>
+				<?php foreach($content['information-links'] as $link) { ?>
+					<h3 class='calm-voice'><?=$link['heading']?></h3>
+					<ul>
+						<?php foreach($link['links'] as $link_name) { ?>
+							<li>
+								<a href='#'><?=$link_name['name']?>
+								</a>
+							</li>
+						<?php } ?>
+					</ul>
 				<?php } ?>
-			</ul>
-		<?php } ?>
-	</nav>
+			</nav>
 
-	<nav class='site-menu company'>
-		<?php foreach($content['company-links'] as $link) { ?>
-			<h3><?=$link['heading']?></h3>
-			<ul>
-				<?php foreach($link['links'] as $link_name) { ?>
-					<li>
-						<a href='#'><?=$link_name['name']?>
-						</a>
-					</li>
+			<nav class='site-menu company'>
+				<?php foreach($content['company-links'] as $link) { ?>
+					<h3 class='calm-voice'><?=$link['heading']?></h3>
+					<ul>
+						<?php foreach($link['links'] as $link_name) { ?>
+							<li>
+								<a href='#'><?=$link_name['name']?>
+								</a>
+							</li>
+						<?php } ?>
+					</ul>
 				<?php } ?>
-			</ul>
-		<?php } ?>
-	</nav>
+			</nav>
 
-	<form class='subscribe'>
-		<fieldset>
-			<?php foreach($content['subscribe-section'] as $form_text) { ?>
-				<h3><?=$form_text['heading']?></h3>
-
-				<!-- insert form component-->
-
-				<p><?=$form_text['fine-print']?></p>
-			<?php } ?>
-		</fieldset>
-	</form>
-</site-map>
-<ul class='footer-masthead'>
-	<li>
-		<a href="#" class='link logo'><?php include('templates/components/logo.php')?>
-		</a>
-	</li>
-	<li>
-		<nav class='page-menu'>
-			<?php include('templates/partials/page-footer-nav.php')?>
-		</nav>
-	</li>
-	<li>
-		<ul class='socials'>
+			<form class='site-menu subscribe' method='POST'>
+				<fieldset>
+					<?php foreach($content['subscribe-section'] as $form_text) { ?>
+						<h3 class='calm-voice'><?=$form_text['heading']?></h3>
+						<?php include('templates/components/email-input.php') ?>
+						<p><?=$form_text['fine-print']?></p>
+					<?php } ?>
+				</fieldset>
+			</form>
+			
+		</site-map>
+		<ul class='footer-masthead'>
 			<li>
-				<a href='#'><?php include('templates/components/icon.php')?></a>
+				<a href="#" class='logo'><?php include('templates/components/logo.php')?>
+				</a>
 			</li>
 			<li>
-				<a href='#'><?php include('templates/components/icon.php')?></a>
+				<nav class='page-menu'>
+					<?php include('templates/partials/page-footer-nav.php')?>
+				</nav>
 			</li>
 			<li>
-				<a href='#'><?php include('templates/components/icon.php')?></a>
+				<ul class='socials'>
+					<li>
+						<a href='#'><?php include('templates/components/icon.php')?></a>
+					</li>
+					<li>
+						<a href='#'><?php include('templates/components/icon.php')?></a>
+					</li>
+					<li>
+						<a href='#'><?php include('templates/components/icon.php')?></a>
+					</li>
+				</ul>
 			</li>
 		</ul>
-	</li>
-<?php } ?>
+	<?php } ?>
+<inner-column>
+
