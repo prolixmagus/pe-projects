144e3ec2a26a1f47ce5c553022c94494036e099d
prolixmagus <jeremypfishman@gmail.com>
2024-03-08 01:06:51 -0800

    Create email form component

---
+++ projects/site-mockup-to-code/templates/components/email-input.php
@@ -0,0 +1,7 @@
+<label for='email'></label>
+<div class='email-input'>
+	<input class='email' id='email' type='email' name='email' placeholder='Email address' required />
+	<button type='submit' name='submitted'>
+		⟩
+	</button>
+</div>
