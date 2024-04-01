520f0bc5561ca0b0ca2a63604d42fb82402a43f8
prolixmagus <jeremypfishman@gmail.com>
2024-04-01 15:41:09 -0700

    Add call-to-action-2 section

---
+++ projects/theme-challenge/modules/call-to-action-2/template.php
@@ -0,0 +1,9 @@
+
+<call-to-action>
+	<h2 class='heading attention-voice'><?=$section['heading']?></h2>
+
+	<p class='story'><?=$section['copy']
+?></p>
+
+	<a class='button' href='#'><?=$section['button']?></a>
+</call-to-action>
