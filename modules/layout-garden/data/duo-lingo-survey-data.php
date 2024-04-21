9bbe54245b94728e33deff29c4db54a8f35b7dcf
prolixmagus <jeremypfishman@gmail.com>
2024-04-20 21:08:56 -0700

    Add new layout to layout-garden

---
+++ modules/layout-garden/data/duo-lingo-survey-data.php
@@ -0,0 +1,37 @@
+<?php
+
+$categories = [
+	[
+		"category" => "TV",
+		"id" => "tv",
+	],
+	[
+		"category" => "Facebook/Instagram",
+		"id" => "socials",
+	],
+	[
+		"category" => "News/article/blog",
+		"id" => "news",
+	],
+	[
+		"category" => "TikTok",
+		"id" => "tiktok",
+	],
+	[
+		"category" => "YouTube",
+		"id" => "youtube",
+	],
+	[
+		"category" => "Friends/family",
+		"id" => "friends",
+	],
+	[
+		"category" => "Google Search",
+		"id" => "search",
+	],
+	[
+		"category" => "Other",
+		"id" => "other",
+	],
+]
+?>
---
+++ modules/layout-garden/duo-lingo-survey/article-card/template.php
@@ -0,0 +1,9 @@
+<li class="user-sources-item">
+	<svg class='duo-icon' viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">
+  		<rect width="150" height="100" />
+	</svg>
+	<field>
+		<input type=checkbox id='<?=$category['id']?>'></input>
+		<label for='<?=$category['id']?>'><?=$category['category']?></label>
+	</field>
+</li>
