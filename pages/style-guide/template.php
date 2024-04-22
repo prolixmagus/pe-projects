ceeb042a1edb6b9af9188843d413444edc4a68dd
prolixmagus <jeremypfishman@gmail.com>
2024-04-22 01:34:23 -0700

    Add initial files to style guide

---
+++ pages/style-guide/template.php
@@ -0,0 +1,11 @@
+<section>
+	<inner-column>
+		<?php include ('modules/nav-menu/template.php')?>
+	</inner-column>
+</section>
+
+<section>
+	<inner-column>
+		<?php include ('modules/exercise-card/template.php')?>
+	</inner-column>
+</section>
