c5983587945c1b05d57a7b7e0d0678c438274abc
prolixmagus <jeremypfishman@gmail.com>
2024-04-24 04:09:56 -0700

    Add typeface module

---
+++ modules/typeface/template.php
@@ -0,0 +1,43 @@
+<typeface class='lettering'>
+	<ul>
+		<li>abcdefghijklmnopqrstuvwxyz</li>
+		<li>ABCDEFGHIJKLMNOPQRSTUVWXYZ</li>
+		<li>(!@#$%&.^,*?:;)</li>
+	</ul> 
+</typeface>
+
+<voice>
+	<ul>
+		<li>
+			<lettering-voice>
+				<h3 class='quiet-voice'>quiet-voice</h3>
+				<p class='quiet-voice'>11px || 0.6875rem || weight 500</p>
+			</lettering-voice>
+		</li>
+		<li>
+			<lettering-voice>
+				<h3 class='calm-voice'>calm-voice</h3>
+				<p class='quiet-voice'>16px || 1rem || weight 500</p>
+			</lettering-voice>
+		</li>
+		<li>
+			<lettering-voice>
+				<h3 class='strong-voice'>strong-voice</h3>
+				<p class='quiet-voice'>16px || 1rem || weight 700</p>
+			</lettering-voice>
+		</li>
+		<li>
+			<lettering-voice>
+				<h3 class='attention-voice'>attention-voice</h3>
+				<p class='quiet-voice'>16px || 1.5rem || weight 700</p>
+			</lettering-voice>
+		</li>
+		<li>
+			<lettering-voice>
+				<h3 class='loud-voice'>loud-voice</h3>
+				<p class='quiet-voice'>16px || 2rem || weight 700</p>
+			</lettering-voice>
+		</li>
+	</ul>
+</voice>
+
