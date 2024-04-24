f1ff48b3780e8170a69844c77bc10217291b9d77
prolixmagus <jeremypfishman@gmail.com>
2024-04-24 04:10:09 -0700

    Add generic-text-module

---
+++ modules/generic-text-module/template.php
@@ -0,0 +1,7 @@
+<?php
+	$content = $content ?? "<h2 class='attention-voice'>Generic Text Heading</h2><p>This is a generic module where you can add any html text elements. Perhaps it is used for a module that needs to be slighlty customized in different areas?</p>"
+?>
+
+<generic-text-heading>
+	<?=$content?>
+</generic-text-heading>
