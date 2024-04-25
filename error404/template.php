976ec8fad289330a9c547ae21c5e6f884fe89713
prolixmagus <jeremypfishman@gmail.com>
2024-04-25 01:56:49 -0700

    Add wrapper

--- error404/template.php
+++ error404/template.php
@@ -1,3 +1,5 @@
-<h2 class="loud-voice">Nothing to see here!</h2> 
+<error404>
+	<h2 class="loud-voice">Nothing to see here!</h2> 
 
-<p class="calm-voice">Note-to-self: find a relaxing background image to chase away the empty void.</p>
+	<p class="calm-voice">Note-to-self: find a relaxing background image to chase away the empty void.</p>
+</error404>
