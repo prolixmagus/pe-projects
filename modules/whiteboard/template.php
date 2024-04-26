5eeee0abb83b223dcf9df871d2ed33ec03a3b7fc
prolixmagus <jeremypfishman@gmail.com>
2024-04-26 16:36:28 -0700

    Add files for whiteboard application

--- modules/whiteboard/template.php
+++ modules/whiteboard/template.php
@@ -1,2 +1,16 @@
-<canvas id='whiteboard' width="500" height="500">
-</canvas>
+
+<white-board>
+	<color-button-container class='canvas-btns'>
+		<input id='color-picker' type='color'>
+		<button id='red-btn'></button>
+		<button id='blue-btn'></button>
+		<button id='yellow-btn'></button>
+		<button id='rainbow-btn'></button>
+		<button id='clear-btn'>
+			<svg class='eraser-icon' fill="none" height="40" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m11.1968 2.43913c.5858-.58579 1.5355-.58579 2.1213 0l4.2426 4.24264c.5858.58578.5858 1.53553 0 2.12132l-8.19687 8.19691h5.13607c.2761 0 .5.2239.5.5s-.2239.5-.5.5h-6.6804c-.40979.0215-.82671-.1243-1.13972-.4373l-4.24264-4.2426c-.58578-.5858-.58578-1.5355 0-2.1213zm1.4142.7071c-.1953-.19526-.5118-.19526-.7071 0l-6.36568 6.36568 4.94978 4.94979 6.3656-6.36572c.1953-.19526.1953-.51184 0-.70711zm-2.83014 12.02257-4.94975-4.9498-1.68686 1.6869c-.19526.1952-.19526.5118 0 .7071l4.24264 4.2426c.19526.1953.51184.1953.70711 0z" fill="#212121"/></svg>
+		</button>
+	</color-button-container>
+	<canvas id='whiteboard'></canvas>
+</white-board>
+
+
---
+++ scripts/home.js
@@ -0,0 +1,124 @@
+
+//get canvas context
+
+var canvas = document.querySelector('canvas')
+var ctx = canvas.getContext("2d");
+var whiteboard = document.querySelector('whiteboard');
+
+// set states
+var drawing = false;
+var rainbow = false;
+var lastColor = ''
+
+//elements
+
+var redBtn = document.getElementById('red-btn')
+var blueBtn = document.getElementById('blue-btn')
+var yellowBtn = document.getElementById('yellow-btn')
+var rainbowBtn = document.getElementById('rainbow-btn')
+var clearBtn = document.getElementById('clear-btn')
+var colorPicker = document.getElementById('color-picker')
+
+var lastX;
+var lastY;
+
+function stopDrawing(event) {
+	drawing = false;
+}
+
+function draw(event) {
+	event.preventDefault();
+	if(drawing) {
+	var rect = canvas.getBoundingClientRect();
+	var currentX = (event.clientX - rect.left) * (canvas.width / rect.width);
+	var currentY = (event.clientY - rect.top) * (canvas.height / rect.height);
+	ctx.lineWidth = 15;
+	if (rainbow) {
+		ctx.strokeStyle = randomHexColor();
+	} else {
+      ctx.strokeStyle = colorPicker.value;
+   }
+	ctx.beginPath();
+	ctx.moveTo(lastX, lastY);
+	ctx.lineTo(currentX, currentY);
+	ctx.stroke(); 
+	ctx.closePath();
+	lastX = currentX;
+	lastY = currentY;
+	}
+
+}
+
+//repetitive ... can initialize?
+function startDrawing(event) {
+	drawing = true;
+	//getting position relative to canvas box
+	var rect = canvas.getBoundingClientRect();
+	lastX = (event.clientX - rect.left) * (canvas.width / rect.width);
+	lastY = (event.clientY - rect.top) * (canvas.height / rect.height);
+	ctx.beginPath();
+}
+
+function randomHexColor() {
+	var hexString = '#'
+	var hexDigits = '123456789abcdef';
+
+	for(let i = 0; i < 6; i++) {
+		hexString += (hexDigits[Math.floor(Math.random() * hexDigits.length)])
+	}
+
+	return hexString;
+}
+
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+//to account for touch and mouse events
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("touchstart", startDrawing);
+canvas.addEventListener("touchmove", draw);
+canvas.addEventListener("touchend", stopDrawing);
+
+//Event listeners for adding different stroke styles to each button
+redBtn.addEventListener('click', function() {
+	ctx.strokeStyle = "#FF0000";
+	colorPicker.value = "#FF0000";
+	rainbow = false; // this is what I needed to allow you to swap between colors on click!
+	rainbowBtn.innerHTML = ``
+})
+
+yellowBtn.addEventListener('click', function() {
+	ctx.strokeStyle = "#FFFF00";
+	colorPicker.value = "#FFFF00";
+	rainbow = false;
+	rainbowBtn.innerHTML = ``
+})
+
+blueBtn.addEventListener('click', function() {
+	ctx.strokeStyle = "#0000FF";
+	colorPicker.value = "#0000FF";
+	rainbow = false;
+	rainbowBtn.innerHTML = ``
+})
+
+// changing stroke to 
+colorPicker.addEventListener('change', function(event) {
+	ctx.strokeStyle = event.target.value;
+	lastColor = event.target.value;
+})
+
+function toggleRainbowMode(event) {
+	rainbow = !rainbow
+
+}
+rainbowBtn.addEventListener('click', function() {
+	if (rainbow = true) {
+		rainbowBtn.innerHTML = `<p>On</p>`
+		colorPicker.value = `#FFFFFF`
+	}
+})
+
+//clears the canvas
+clearBtn.addEventListener('click', clearCanvas);
