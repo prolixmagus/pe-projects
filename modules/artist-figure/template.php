0a2f957fde9839bf02b906f969ea05ab233290f8
prolixmagus <jeremypfishman@gmail.com>
2024-04-29 00:42:03 -0700

    Create artist-figure module

---
+++ modules/artist-figure/template.php
@@ -0,0 +1,19 @@
+<?php 
+	$figure = $creator['figure'] ?? "images/landscape.jpg";
+	$figCaption = $creator['figCaption'] ?? "An illuminating piece of information or a pithy aside.";
+	$creatorName = $creator['creator'] ?? "The name of the author, musician, director, network, or artist."
+	
+	?>
+
+<artist-figure>
+	<figure>
+		<!--<artist-icon><svg></svg></artist-icon>-->
+		<picture>
+			<img src='<?=$figure?>' alt="[[todo]]">
+		</picture>
+			<figcaption>
+				<h2><?=$figCaption?></h2>
+				<p><?=$creatorName?></p>
+			</figcaption>
+	</figure>
+</common-figure>
