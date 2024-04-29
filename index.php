ca764db10f1a68fddc2dd1b45ec9ae0291325b20
prolixmagus <jeremypfishman@gmail.com>
2024-04-29 00:43:35 -0700

    Refactor index to account for php modules

--- index.php
+++ index.php
@@ -6,27 +6,25 @@ error_reporting(E_ALL);
 <?php include("modules/site-header/template.php");?>
 
 	<main>
-		<section class='<?=$page?>'>		
-			<?php get_template($page) ?>	
+		<section class='<?=$page?>'>	
+			<?php get_template($page) ?>
 		</section>
 	</main>
 
-	<footer>
-		<inner-column>
-			<?php include("modules/footer/template.php")?>
-		</inner-column>
-	</footer>
+
+<?php include("modules/footer/template.php")?>
 
 
-	<script src="<?=get_site_scripts($page)?>" type='module'></script>
 
-	<?php if (isset($_GET['page']) && $_GET['page'] == "exercise") {
-		foreach($exercises as $exercise) {
-			if ($_GET['slug'] == $exercise["slug"]) { ?>
-				<script src="<?=get_exercise_scripts($exercise["slug"])?>"></script>
-			<?php } 
-		} 
-	} ?>
+<script src="<?=get_site_scripts($page)?>" type='module'></script>
+
+<?php if (isset($_GET['page']) && $_GET['page'] == "exercise") {
+	foreach($exercises as $exercise) {
+		if ($_GET['slug'] == $exercise["slug"]) { ?>
+			<script src="<?=get_exercise_scripts($exercise["slug"])?>"></script>
+		<?php } 
+	} 
+} ?>
 
 
 	</body>
