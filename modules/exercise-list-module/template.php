6acba292c885d323fac841e42c74a159f8776583
prolixmagus <jeremypfishman@gmail.com>
2024-04-29 00:45:06 -0700

    Delete files

--- modules/exercise-list-module/template.php
+++
@@ -1,9 +0,0 @@
-<?php $exercises = read_page_data('exercises-database');?>
-
-<exercise-list-module>
-	<ol class='project-list'>
-		<?php foreach($exercises as $exercise) { 
-			include('modules/exercise-card/template.php');
-		} ?>
-	</ol>
-</exercise-list-module>
