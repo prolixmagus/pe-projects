d96cb9f5b35d8e483ae5d6064da1a73da098e782
prolixmagus <jeremypfishman@gmail.com>
2024-04-29 17:09:38 -0700

    Delete files

--- error404/template.php
+++
@@ -1,5 +0,0 @@
-<error404>
-	<h2 class="loud-voice">Nothing to see here!</h2> 
-
-	<p class="calm-voice">Note-to-self: find a relaxing background image to chase away the empty void.</p>
-</error404>
