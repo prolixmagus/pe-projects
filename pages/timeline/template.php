ec2e62d7a510346d54cb8dfeeedd0359e390cd96
prolixmagus <jeremypfishman@gmail.com>
2024-04-29 17:11:21 -0700

    Convert data to JSON and add PHP logic

--- pages/timeline/template.php
+++ pages/timeline/template.php
@@ -1,178 +1,41 @@
+<?php $timelineData = read_page_data('timeline') ?>
+
+<?php
+	$pageTitle = $timelineData['title'];
+	$pageIdea = $timelineData['intro'];
+?>
 
 <section class='intro'>
 	<inner-column>
-		<h2 class='loud-voice'>Everything I remember so far</h2>
+
+		<?php include('modules/page-header/template.php');?>
 
 	</inner-column>
 </section>
 
-<section class='timeline' id='present-day'>
-	<inner-column class='first'>
-		<work-narrative>
-			<h2 class='attention-voice'>2024</h2>
+<?php foreach($timelineData['section'] as $section) { ?>
+	<section class='<?=$section['id']?>' id='<?=$section['id']?>'>
+		<inner-column class='first'>
+			<work-narrative>
+				<h2 class='attention-voice'><?=$section['year']?></h2>
 
-			<p>I am currently enrolled in a web development course by <a class='derek-pink' href='https://perpetual.education/'>Perpetual Education</a> called Design for <em>the Web</em>.</p>
-
-			<p>It's a lot of work. I love it.</p>
-
-			<p>Aside from researching and applying new web technologies, I love reading (or writing) short stories and playing jazz. Feel free to check out what I'm currently reading, listening to, or watching.</p>
-		</work-narrative>
-
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-
-			<ul title='skills so far'>
-				<li>Research and study through <a href='https://www.sciencedirect.com/topics/psychology/deliberate-practice'>Deliberate Practice</a></li>
-				<li>Preparing shareable rich metadata assets</li>
-				<li>Creating and exporting original graphics using Affinity 2 design software</li>
-				<li>Formatting responsive webpages for accessibility via semantic HTML, typography, and usability concerns</li>
-				<li>Managing a resilient design system using custom properties, JSON data, and PHP modules</li>
-				<li>HTML + CSS + PHP + Wordpress</li>
-			</ul>
-		</experience-gained>
-
-	</inner-column>
-</section>
-
-<section class='timeline' id='burbank'>
-	<inner-column>
-		<work-narrative>
-			<h2 class='attention-voice'>2022</h2>
-			
-			<p>Four years and one pandemic later, I am flying across the country to teach at John Muir Middle School in Burbank, California.</p>
-
-		</work-narrative>
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Designed over 185 unique standards-based lesson plans</li>
-				<li>Tracked student progress / behavior for ~100 English Language Learners</li>
-				<li>Developed academic and behavior intervention plans alongisde counselors and school psychologists</li>
-				<li>Proctored high stakes state tests (CAASPP and ELPAC)</li>
-				<li>Shared ELD best practices with Math, Science, and History departments</li>
-			</ul>
-		</experience-gained>
-	</inner-column>
-</section>
-
-<section class='timeline' id='white-oak'>
-	<inner-column>
-		<work-narrative>
-			<h2 class='attention-voice'>2019</h2>
-			
-			<p>I'm back in the United States teaching English as a Second language to immigrants from all over the world. I'm teaching at White Oak Middle School in Maryland.</p>
-
-		</work-narrative>
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Co-taught STEM courses as ELD support</li>
-				<li>Created language curriculum for ELD levels 1-4 and 6th-8th ELA classes.</li>
-				<li>Proctored WIDA assessment to place incoming and current students</li>
-			</ul>
-		</experience-gained>
-	</inner-column>
-</section>
-
-	<!-- insert section for Burbank Middle School / Sylvan and White Oak -->
-
-<section class='timeline'>
-	<inner-column>
-		<work-narrative>
-			<h2 class='attention-voice'>2018</h2>
-			
-			<p>I am in teaching in English in Taiwan as the recipient of a Fulbright Scholarship. I travel to different elementary schools in Zhiben, a spa town in the Taitung region.</p>
-
-			<p>Here is an archive of a blog I wrote during my stay</p>
-			
-			
-			<!-- insert link here -->
-		</work-narrative>
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Co-taught grades K-6 EFL classes with a certified local English teacher</li>
-				<li>Initiated and delivered assessments to improve student cooperation, critical thinking, and communication skills</li>
-				<li>Participated in local and government events as a cultural ambassador</li>
-			</ul>
-		</experience-gained>
-	</inner-column>
-</section>
+				<?=$section['narrative']?>
+			</work-narrative>
 
 
-<section class='timeline'>
-	<inner-column>
-		<work-narrative>
-			<h2 class='attention-voice'>2017</h2>
-			
-			<p>After three years, I graduate from Montclair State University with a Master's in Teaching English as a Second Language. I am one course shy of a Master's in Applied Linguistics (statistics)</p>
-			
-			<p>Post-graduation, I take the train to Princeton, <abbr title='New Jersey'>NJ</abbr>, to write test questions for the <abbr title='Test of English as a Foreign Language'>TOEFL</abbr> <abbr title='internet-Based-Test'>iBT</abbr> at <abbr title='Educational Testing Services'>ETS</abbr> over the summer.</p>
-		</work-narrative>
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Researched, created, and revised test items for the Speaking Section of the TOEFL iBT</li>
-				<li>Researched and cross-referenced data</li>
-				<li>Safeguarded confidential testing assets</li>
-			</ul>
-		</experience-gained>
-	</inner-column>
-</section>
+			<experience-gained>
+				<h3 class='strong-voice'>Skills</h3>		
+
+				<ul title='skills'>
+					<?php foreach($section['skills'] as $skill) { ?>
+						<li><?=$skill['skill']?></li>
+					<?php } ?>
+				</ul>
+			</experience-gained>
 
 
-<section class='timeline'>
-	<inner-column>
-		<work-narrative>
-			<h2 class='attention-voice'>2014</h2>
-			
-			<p>I disembark a plane that just landed in Seattle, Washington, to search for new prospects in the midst of a recession. A few months later and I am working the help line at Nintendo of America's call center.</p>
-			
-			<p>I soon get promoted to become an Associate Data Analyst.</p> 
-			
-			<p>I feel stuck. I miss my family. I move back home</p>
-		</work-narrative>
-		<experience-gained>
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Compiled feedback from tech forums, social media, news outlets, and the Miiverse into digestible reports for upper management.</li>
-				<li>Created data-driven flash reports tracking user reactions to software updates and press releases.</li>
-			</ul>
-		</experience-gained>
-	</inner-column>
-</section>
-		
-<section class='timeline'>
-	<inner-column class='last'>
-		<work-narrative>
-			
-			<h2 class='attention-voice'>2011</h2>
-			
-			<p>Clutching a degree in Creative Writing from the University of Rochester and a certificate from NYU's Summer Publishing Institute, I set out to realize my dreams of becoming an Editor.</p>
-			
-			<p>For one year, I am an Assistant Editor of E-Books at Skyhorse Publishing.</p> 
-			
-			<p>This is the year the publishing industry implodes.</p>
+		<?php include('modules/common-figure/template.php')?>
 
-		</work-narrative>
-		<experience-gained>
-			
-			<h3 class='strong-voice'>skills</h3>
-			
-			<ul>
-				<li>Proofread and reformatted 60 titles per month for online distribution to vendors such as Amazon, Barnes & Noble, and iTunes Bookstore</li>
-				<li>Compiled and distributed Metadata excel sheets for over 100 vendors through FTP</li>
-				<li>Tracked sales and inventory from digital marketplace using iTunes Producer and Amazon’s Vendor Central</li>
-			</ul>
-		</experience-gained>
-	</inner-column>	
-</section>
-
-<picture class='seaweed'>
-	<img src='images/seaweed-1.svg' alt='pink seaweed'>
-</picture>
+		</inner-column>
+	</section>
+<?php } ?>
