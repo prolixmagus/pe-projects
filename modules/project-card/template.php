dacd45b54db8b75ef61e9e0fcf69847edeabffb7
prolixmagus <jeremypfishman@gmail.com>
2024-05-07 23:28:12 -0700

    Create project card module

---
+++ modules/project-card/template.php
@@ -0,0 +1,21 @@
+<?php
+	$address = $section['address'] ?? "?";
+	$name = $section['name'] ?? "This is the name of the project";
+	$competency = $section['competency'] ?? "This explains what languages or frameworks used in the project";
+	$description = $section['description'] ?? "This is a description of the project.";
+?>
+
+<project-card class='exercise e4p'>
+	<?php include('modules/common-figure/template.php')?>
+
+	<a href='<?=$address?>'>
+		<h2><?=$name?></h2>
+	</a>
+
+	<h3 class='calm-voice'><?=$competency?></h2>
+
+	<p><?=$description?></p>
+
+	<a href='<?=$address?>'>Read more</a>
+</project-card>
+
