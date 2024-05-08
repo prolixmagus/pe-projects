a375fe24506a2c37abb93b0851d2bf3eac504da9
prolixmagus <jeremypfishman@gmail.com>
2024-05-07 23:29:52 -0700

    Make default values for module

--- modules/goals-list/template.php
+++ modules/goals-list/template.php
@@ -1,7 +1,10 @@
+<?php $section['goals'] = $section['goals'] ?? [1, 2, 3, 4];?>
+
 <ul class='goal'>
 		<?php foreach($section['goals'] as $goal) { ?>
-			<li>
-				<p><?=$goal['hope']?></p>
-			</li>
+			<?php $goal = $goal['hope'] ?? "My aspirations and career goals are listed here!"?>
+				<li>
+					<p><?=$goal?></p>
+				</li>
 		<?php } ?>
 </ul>
