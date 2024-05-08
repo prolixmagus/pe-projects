a0d33900b4735fecfe2fd1d9d4cba9b15928e601
prolixmagus <jeremypfishman@gmail.com>
2024-05-07 23:29:37 -0700

    Adjust variables in module

--- modules/exercise-card/template.php
+++ modules/exercise-card/template.php
@@ -1,15 +1,19 @@
 <?php
-	$exercise_name = $exercise['name'] ?? "Exercise name";
+	$name = $exercise['name'] ?? "Exercise name";
 	$exercise_description = $exercise['description'] ?? "Here is a description of an exercise!";
-	$exercise_slug = $exercise['slug'] ?? "hello-world"
+	$exercise_slug = $exercise['slug'] ?? "hello-world";
+	$address = $exercise['address'] ?? "#";
 ?>
 
 <exercise-card class='exercise'>
-	<a class='exercise-link' href='?page=exercise&slug=<?=$exercise_slug?>'>
-		<?=$exercise_name?>
+	<a class='exercise-link' href='<?=$address?>'>
+		<h2><?=$name?></h2>
 		<div class='e4p-icon'>
 			<?php include('icons/' . $exercise_slug . '.svg')?>
 		</div>
 	</a>
+	
 	<p><?=$exercise_description?></p>
+
+	<a class='exercise-link' href='<?=$address?>'>Read more</a>
 </exercise-card>
