4a257f16d3f56c4fc123f76b8cda1a39d213295a
prolixmagus <jeremypfishman@gmail.com>
2024-05-09 01:35:22 -0700

    Apply grid and subgrid to goals page

--- pages/goals/template.php
+++ pages/goals/template.php
@@ -1,11 +1,15 @@
-<?php $goalsData = read_page_data('goals') ?>
+<?php $pageData = read_page_data('goals'); 
+	$pageTitle = $pageData['title'];
+	$pageIdea = $pageData['idea'];
+?>
+
 <?php include('modules/page-header/template.php') ?>
 
-<?php foreach ($goalsData['sections'] as $section) {
+<?php foreach ($pageData['sections'] as $section) {
 	$module = $section['module'];
 ?>
-<section>
-	<inner-column>
+<section class='section-grid'>
+	<inner-column class='inner-grid'>
 		<?php include("modules/$module/template.php")?>
 	</inner-column>
 </section>
