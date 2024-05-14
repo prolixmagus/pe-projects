46552b8535464ef2ea2a91c92cae56a886b7fce7
prolixmagus <jeremypfishman@gmail.com>
2024-05-13 18:15:23 -0700

    Update footer style

--- modules/footer/template.php
+++ modules/footer/template.php
@@ -1,5 +1,6 @@
 <footer class='site-footer section-grid'>
 	<inner-column class='inner-grid'>
 		<?php include('modules/site-map/template.php')?>
+		<p class='quiet-voice caption updated-time'><em>Last Updated: Saturday, May 11th, 2024</em></p>
 	</inner-column>
 </footer>
