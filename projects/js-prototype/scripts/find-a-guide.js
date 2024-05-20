44c768ca4a057e6e372926b9286ea3b6db0693d9
prolixmagus <jeremypfishman@gmail.com>
2024-05-20 16:27:37 -0700

    Add login page render

---
+++ projects/js-prototype/scripts/find-a-guide.js
@@ -0,0 +1,46 @@
+const body = document.querySelector('body');
+
+function renderLoginPage() {
+  body.innerHTML = 
+    `
+      <h1 class='loud-voice'>Find-A-Guide</h1>
+      <section class='login-page'>
+
+        <form class='login'>
+         <label for='userEmail'>Email</label>
+         <input id='userEmail' type='email' class='email' placeholder='someone@something.com' required>
+
+         <label for='password'>Password</label>
+         <input id='password' class='password' type='password' placeholder='Enter password' required>
+
+         <button class='login-btn' type='button'>Login</button>
+        </form>
+
+        <p class='message-container'></p>
+
+      </section>
+    `
+
+const loginButton = document.querySelector('.login-btn');
+const passwordInput = document.querySelector('.password');
+const messageContainer = document.querySelector('.message-container');
+  
+attachLoginEventListener(loginButton, passwordInput, messageContainer);
+}
+
+function attachLoginEventListener(button, password, message) {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    message.innerHTML = '';
+    
+    if (password.value === 'dragon') {
+      message.innerHTML = `You did it!`
+    } else {
+      message.innerHTML = `Try again!`;
+    }
+    
+  })
+}
+
+renderLoginPage();
