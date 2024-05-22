a740f74c106f971e51516a7f0415593278ad7245
prolixmagus <jeremypfishman@gmail.com>
2024-05-21 17:50:11 -0700

    Add file for state object

--- projects/js-prototype/scripts/site-footer.js
+++ projects/js-prototype/scripts/site-footer.js
@@ -1,17 +1,26 @@
-import {renderNavLinks} from './site-header.js'
+import {renderHeaderLinks, attachLinkEventListeners} from './site-header.js'
+import { state } from './state.js';
 
 function renderSiteFooter(container) {
-  container.innerHTML += ` 
+  const footer = ` 
     <footer class='site-footer'>
       <inner-column>
         <nav class='user-menu'>
           <ul>
-            ${renderNavLinks()}
+            ${renderHeaderLinks()}
           </ul>
         </nav>
       </inner-column>
     </footer>
   `
+  container.insertAdjacentHTML('afterend', footer);
+
+  const searchNav = document.querySelector('.site-footer button[data-link="search"]')
+  const inboxNav = document.querySelector('.site-footer button[data-link="inbox"]')
+  const tripsNav = document.querySelector('.site-footer button[data-link="trips"]')
+  const profileNav = document.querySelector('.site-footer button[data-link="profile"]')
+
+  attachLinkEventListeners(searchNav, inboxNav, tripsNav, profileNav)
 }
 
 export {
---
+++ projects/js-prototype/scripts/state.js
@@ -0,0 +1,7 @@
+const state = {
+	login: false;
+}
+
+export {
+	state
+}
