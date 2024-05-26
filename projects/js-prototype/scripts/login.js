f1ba34d142feb7b65b68d56c2ed94bb062ecf3e9
prolixmagus <jeremypfishman@gmail.com>
2024-05-25 17:47:11 -0700

    Add login feature that keeps info in localstorage

--- projects/js-prototype/scripts/login.js
+++ projects/js-prototype/scripts/login.js
@@ -12,13 +12,13 @@ function renderLoginView(container) {
 
           <form class='login' data-action='login'>
             <field>
-              <label for='userEmail'>Email</label>
-              <input id='userEmail' type='email' class='email' value='charlesmings@jazz.org' required>
+              <label for='username'>Username</label>
+              <input id='username' class='username' type='text' required>
             </field>
 
             <field>
               <label for='password'>Password</label>
-              <input id='password' class='password' type='password' value='moanin' required>
+              <input id='password' class='password' type='password' required>
             </field>
 
             <button class='login-btn' data-set="login" type='submit'>Submit</button>
@@ -33,35 +33,54 @@ function renderLoginView(container) {
   attachLoginFormEventListener()
 }
 
-function validate(password) {
-  return (password.value === 'moanin')
+function validate(password, storedUserPassword, username, storedUserInput) {
+  return (password.value === storedUserPassword && username.value === storedUserInput)
+}
+
+function createUserProfile(username, userPassword) {
+  const userData = { username: username.value, password: userPassword.value };
+  localStorage.setItem(userData.username, JSON.stringify(userData));
+}
+
+function getUserData(key) {
+  return localStorage.getItem(key)
 }
 
 function handleLogin() {
+  const username = document.querySelector('.username')
   const password = document.querySelector('.password');
   const message = document.querySelector('.login-message');
 
-  if (validate(password)) {
-    const main = document.querySelector('main');
-    state.login = true;
-    renderSiteHeader(main);
-    attachLinkEventListeners();
-    attachTemplate(getTourSearchView);
-    renderSiteFooter(main);
+  const userData = getUserData(username.value)
+
+  if (userData) {
+    const storedUser = JSON.parse(userData);
+    if ( validate(password, storedUser.password, username, storedUser.username) ) {
+      const main = document.querySelector('main');
+      state.login = true;
+      renderSiteHeader(main);
+      attachLinkEventListeners();
+      attachTemplate(getTourSearchView);
+      renderSiteFooter(main);
+    } else {
+      message.textContent = 'Incorrect password';
+    }
   } else {
-    message.textContent = 'Incorrect password';
+    createUserProfile(username, password)
   }
 }
 
 function attachLoginFormEventListener() {
-  window.addEventListener('click', (event) => {
+  const loginForm = document.querySelector('.login')
+
+  loginForm.addEventListener('click', (event) => {
+    event.preventDefault();
     if (event.target.matches('[data-set="login"]') ) {
       event.preventDefault();
       handleLogin();
     }
   })
 }
-
 
 export {
 	renderLoginView,
