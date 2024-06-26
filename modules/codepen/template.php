3b7c3f1a71d890ddc6e3e97f4d63deed9e973c7f
prolixmagus <jeremypfishman@gmail.com>
2024-06-26 01:24:00 -0700

    Added codepen module to styleguide

--- modules/codepen/template.php
+++ modules/codepen/template.php
@@ -1,7 +1,8 @@
 <?php
-	$pen = $section['pen'] ?? "This holds a CodePen for your viewing pleasure."
+	$pen = $section['pen'] ?? "<iframe height='450' style='width: 100%;' scrolling='no' title='caesar-cipher' src='https://codepen.io/Jeremy-Fishman/embed/LYoLLVe?default-tab=result&editable=true' frameborder='no' loading='lazy' allowtransparency='true' allowfullscreen='true'>See the Pen <a href='https://codepen.io/Jeremy-Fishman/pen/LYoLLVe'>caesar-cipher</a> by Jeremy Fishman (<a href='https://codepen.io/Jeremy-Fishman'>@Jeremy-Fishman</a>)on <a href='https://codepen.io'>CodePen</a>.</iframe>";
 ?>
 
 <section class='formatted-codepen'>
+	<h2>This is a sample CodePen!</h2>
 	<?=$pen?>
 </section>
