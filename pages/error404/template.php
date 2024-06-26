274a102454ec2539f8bf82ca8ff0ac427778984b
prolixmagus <jeremypfishman@gmail.com>
2024-06-25 17:55:50 -0700

    Fix layout garden and error404

--- pages/error404/template.php
+++ pages/error404/template.php
@@ -1,7 +1,7 @@
 <section class='section-grid'>
 	<inner-column class='inner-grid'>
 		<div class='error404'>
-			<h2 class="loud-voice">Nothing to see here!</h2> 
+			<h1 class="loud-voice">Nothing to see here!</h1> 
 
 			<p class="calm-voice">Note-to-self: find a relaxing background image to chase away the empty void.</p>
 		</div>
