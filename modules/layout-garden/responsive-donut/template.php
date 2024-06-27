0671e443892cc35fc9ad118e4b01cb843d69cd2d
prolixmagus <jeremypfishman@gmail.com>
2024-06-27 13:44:26 -0700

    Add responsive layout module

---
+++ modules/layout-garden/responsive-donut/template.php
@@ -0,0 +1,34 @@
+<ul class='donut-list'>
+	<li class='featured-donut'>
+		<picture>
+			<img src='modules/layout-garden/responsive-donut/glazed-donut.jpg' alt='yellow glazed donut'>
+		</picture>
+		<article class='donut-description'>
+			<h2 class='donut-voice'>Glazed Donut</h2>
+			<p class='calm-voice'>Praise the glaze! This indulgence will have you shouting for joy.</p>
+			<a class='shop-link' href='#'>Pick Me!</a>
+		</article>
+	</li>
+
+	<li class='featured-donut'>
+		<picture>
+			<img src='modules/layout-garden/responsive-donut/strawberry-donut.jpg' alt='strawberry donut'>
+		</picture>
+		<article class='donut-description'>
+			<h2 class='donut-voice'>Strawberry Donut</h2>
+			<p class='calm-voice'>Our newest testament to berry good flavor. Like turning water into wine!</p>
+			<a class='shop-link' href='#'>Pick Me!</a>
+		</article>
+	</li>
+
+	<li class='featured-donut'>
+		<picture>
+			<img src='modules/layout-garden/responsive-donut/creampie.jpg' alt='boston creampie donut'>
+		</picture>
+		<article class='donut-description'>
+			<h2 class='donut-voice'>Boston Creampie</h2>
+			<p class='calm-voice'>Milky goodness that should be illegal. Get thee to a bunnery!</p>
+			<a class='shop-link' href='#'>Pick Me!</a>
+		</article>
+	</li>
+</ul>
