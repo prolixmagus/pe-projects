e40bfa5f41b5a91ddf869737612969634e3e5adf
prolixmagus <jeremypfishman@gmail.com>
2024-07-03 01:13:11 -0700

    Add styling for home page and potential feature

--- pages/home/template.php
+++ pages/home/template.php
@@ -1,3 +1,5 @@
+<?php $pageData = read_page_data('home');?>
+
 <section class='section-grid'>
 	<inner-column class='inner-grid'>
 		<header class='home-header'>
@@ -8,13 +10,27 @@
 
 				<p class='calm-voice'>If you want to talk tech, music, linguistics, or whatever crosses your mind, you can reach me at <a href='mailto:jeremypfishman@gmail.com'>jeremypfishman@gmail.com</a>. I usually respond within 24 hours.</p>
 			</intro-area>
-			
-			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-			</ul>
 		</header>
+
+		<section class='thoughts'>
+				<h2>Thoughts</h2>
+					<ul class='thoughts-list'>
+						<?php foreach($pageData['blogs'] as $blog) { ?>
+						<li>
+							<a href='<?=$blog['link']?>'>
+								<h3><?=$blog['description']?></h3>
+							</a>
+							<date class='quiet-voice'><?=$blog['date'] ?></date>
+						</li>
+						<?php } ?>
+					</ul>
+		</section>
+
+		<!-- JUST playing ... maybe add to code pen ... do the click button to replace header text, cycle through many options with final message - 'alright you reached the end, curiosity sated? -->
+
+		<div class='disappearing-act'>
+			<button id='disappear' data-action='disappear'>Over it?</button>
+		</div>
 
 		<picture>
 			<img src='../images/jeremy-circles.svg'>
