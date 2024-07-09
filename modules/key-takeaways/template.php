2b90be2998915a754644009028c8443a73373229
prolixmagus <jeremypfishman@gmail.com>
2024-07-09 11:48:55 -0700

    Add js app case study and fix flex on li

--- modules/key-takeaways/template.php
+++ modules/key-takeaways/template.php
@@ -5,7 +5,7 @@
 <key-takeaways class='key-takeaways'>
 	<h2>Key Takeaways</h2>
 
-	<ul>
+	<ul class='points'>
 		<?php foreach ($takeaways as $takeaway) { 
 			$point = $takeaway['teaser'] ?? "A key point ... as a teaser!";
 			$description = $takeaway['description'] ?? "An elaboration of the key point. This module is differentiated for people who want to read more about what interests them or just want the main gist."
