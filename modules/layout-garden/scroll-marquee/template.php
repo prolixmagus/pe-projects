3aefb1ed6478b604f77ac0126ac06eef0392d845
prolixmagus <jeremypfishman@gmail.com>
2024-07-13 01:36:31 -0700

    Add new module to layout garden

---
+++ modules/layout-garden/scroll-marquee/template.php
@@ -0,0 +1,4 @@
+<section class='scroll'>
+  <h2 id='marquee' class='marquee'>Eat Your Veggies
+  </h2>
+</section>
--- public/scripts/layout-garden.js
+++ public/scripts/layout-garden.js
@@ -6,3 +6,18 @@ duoForm.addEventListener('click', (e) => {
 		listItem.classList.toggle('checked')
 	}
 })
+
+const scrollContainer = document.querySelector('.scroll');
+const marquee = document.querySelector('.marquee');
+
+window.addEventListener('scroll', () => {
+  const containerTop = scrollContainer.getBoundingClientRect().top;
+  //check if container is in the viewport
+  if (containerTop < window.innerHeight) {
+    const scrollPos = window.scrollY;
+    const viewportWidth = window.innerWidth;
+    //offsetTop is in relation to the document - not affected by scrolling
+    const textMovePos = (scrollPos - scrollContainer.offsetTop) % (2 * viewportWidth);
+    marquee.style.transform = `translateX(${-textMovePos}px)`;
+  }
+});
