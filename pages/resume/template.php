60b205a0b8960fe7b50167ebb93de84490aee000
prolixmagus <jeremypfishman@gmail.com>
2024-07-18 01:37:07 -0700

    Simplify timeline structure

--- pages/resume/template.php
+++ pages/resume/template.php
@@ -12,28 +12,24 @@
 <?php include('../modules/page-header/template.php');?>
 
 <section>
-		<div class='timeline-wrapper'>
+		<ul class='timeline-wrapper'>
 			<?php foreach($pageData['section'] as $section) {
 				$currentIndex++; 
 			?>
-				<section class='<?=$section['id']?> section-flex' id='<?=$section['id']?>'>
-					<inner-column class='inner-flex'>
 
-						<work-narrative>
-							<h2 class='attention-voice'><?=$section['year']?></h2>
+			<li class='<?=$section['id']?> section-flex' id='<?=$section['id']?>'>
+				<h2 class='attention-voice'><?=$section['year']?></h2>
 
-							<?php 
-								$figure = $section['image'];
-								$figCaption = $section['figCaption'];
+				<?php 
+					$figure = $section['image'];
+					$figCaption = $section['figCaption'];
 
-								include('../modules/common-figure/template.php')
-							?>
+					include('../modules/common-figure/template.php')
+				?>
 
-							<?=$section['narrative']?>
-						</work-narrative>
+				<?=$section['narrative']?>
+			</li>
 
-					</inner-column>
-				</section>
 			<?php } ?>
-		</div>
+		</ul>
 </section>
